6236def7acca35b83ec60e703ade3d57a35dc028
Bin Wang <bin.wang@mail.binwang.me>
2024-10-17 22:14:30 -0400

    init project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to jepsen.postgres-drbd
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/jepsen/postgres_drbd_test.clj
@@ -0,0 +1,7 @@
+(ns jepsen.postgres-drbd-test
+  (:require [clojure.test :refer :all]
+            [jepsen.postgres-drbd :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
